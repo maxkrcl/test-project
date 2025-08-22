dd932b55f1b7c5c65223d9d73da2883b1f28f65d
maxkrcl <138573266+maxkrcl@users.noreply.github.com>
2025-08-22 23:09:13 +0300

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: 'main',
+  extname: 'handlebars'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', async (req, res) => {
+  res.render('home', {
+    title: 'Home',
+    style: '<link rel="stylesheet" href="/css/style.css">',
+    script: '<script src="/js/script.js"></script>'
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
